cdf9371627bd66587130c70bfe1f3cee2d92690f
Lucas Weyne <weynelucas@gmail.com>
2019-08-05 14:04:55 -0300

    Field createdAt

--- src/models/tag.js
+++ src/models/tag.js
@@ -10,6 +10,10 @@ const TagSchema = new mongoose.Schema({
     type: mongoose.Schema.Types.ObjectId,
     ref: 'User',
     required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
   }
 });
 
